dacf30d637f8639623c9bbd15e05c28f34c36887
wkodate <hbwandeow@gmail.com>
2020-07-19 18:59:04 +0900

    add mvc components and test class

--- src/main/java/com/wkodate/tomato/TomatoApplication.java
+++ src/main/java/com/wkodate/tomato/TomatoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TomatoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TomatoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TomatoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/wkodate/tomato/web/TomatoController.java
@@ -0,0 +1,18 @@
+package com.wkodate.tomato.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TomatoController {
+
+    @GetMapping("/")
+    public String index(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                        Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
