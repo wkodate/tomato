e5072956eaad843fc688e5e6fb4b6746f0c5097c
wkodate <hbwandeow@gmail.com>
2020-08-09 23:54:14 +0900

    add progress bar

--- src/main/resources/static/js/timer.js
+++ src/main/resources/static/js/timer.js
@@ -2,10 +2,14 @@ const timer = () => {
   function updateClock() {
     const t = getTimeRemaining(currentTime);
     if (t.total <= 0) {
+      taskCounter++;
+      taskSelector.innerHTML = updateTasks(taskCounter);
+      progressSelector.innerHTML = updateProgress(0);
       clearInterval(timeinterval);
     }
     console.log(t);
-    remaining.innerHTML = ('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2);
+    timerSelector.innerHTML = ('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2);
+    progressSelector.innerHTML = updateProgress((timeInSeconds - currentTime) / timeInSeconds);
     currentTime--;
   }
 
@@ -27,6 +31,26 @@ const getTimeRemaining = (timeInSec) => {
   };
 };
 
+const updateProgress = (rate) => {
+  progress = "";
+  for (i = 0; i < parseInt(10 * rate); i++) {
+    progress += "▓";
+  }
+  for (i = 0; i < 10 - parseInt(10 * rate); i++) {
+    progress += "░";
+  }
+  progress += parseInt(100 * rate) + "%";
+  return progress;
+};
+
+const updateTasks = (cnt) => {
+  tasks = "";
+  for (i = 0; i < cnt; i++) {
+    tasks += "●";
+  }
+  return tasks;
+};
+
 const startstop = () => {
   if (starting) {
     // stop
@@ -45,17 +69,23 @@ const reset = () => {
   starting = false;
   currentTime = timeInSeconds;
   const t = getTimeRemaining(currentTime);
-  remaining.innerHTML = ('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2);
+  timerSelector.innerHTML = ('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2);
 };
 
 const timeInMinutes = 25;
 //const timeInMinutes = 1;
 const timeInSeconds = timeInMinutes * 60;
-const remaining = document.getElementById("timer").querySelector('.remaining');
 const t = getTimeRemaining(timeInSeconds);
 
 let currentTime = timeInSeconds;
+let taskCounter = 0;
+//let progressCounter = 5;
 let timeinterval;
 let starting = false;
 
-remaining.innerHTML = ('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2);
+const timerSelector = document.getElementById("timer").querySelector('.timer');
+timerSelector.innerHTML = ('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2);
+const taskSelector = document.getElementById("timer").querySelector('.tasks');
+taskSelector.innerHTML = updateTasks(taskCounter);
+const progressSelector = document.getElementById("timer").querySelector('.progressBar');
+progressSelector.innerHTML = updateProgress(0);
